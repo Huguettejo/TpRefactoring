a1125a56a5298aaeb1fecd4f10d6f88abc55ef38
Huguettejo <joellehuguetterakotoarisoa@gmail.com>
2024-04-04 12:54:11 +0200

    classe_hummain

--- glog/src/iut/bad/Femme.java
+++ glog/src/iut/bad/Femme.java
@@ -1,25 +1,15 @@
 package iut.bad;
 
-public class Femme {
+public class Femme extends Humain {
    
     private String nom;
     private String prenom;
     private int age;
-
-   
-    public Femme() {
-        
-        this.nom = "";
-        this.prenom = "";
-        this.age = 0;
-    }
-
-  
+    
     public Femme(String nom, String prenom, int age) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
+        super(nom, prenom, age);
     }
+ 
 
    
     public String getNom() {
