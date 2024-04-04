e1aed4f8b64dbcffab5c00c242875165147e59cb
Huguettejo <joellehuguetterakotoarisoa@gmail.com>
2024-04-04 13:53:05 +0200

    ajout de la methode ami dans humain

--- glog/src/iut/bad/Humain.java
+++ glog/src/iut/bad/Humain.java
@@ -1,14 +1,18 @@
 package iut.bad;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Humain implements Consommation {
     protected String nom;
     protected String prenom;
     protected int age;
+    private List<Humain> amis;
 
     public Humain(String nom, String prenom, int age) {
         this.nom = nom;
         this.prenom = prenom;
         this.age = age;
+        this.amis = new ArrayList<>();
     }
 
     public String getNom() {
@@ -50,4 +54,17 @@ public class Humain implements Consommation {
     public String toString() {
         return "Nom: " + nom + ", Prénom: " + prenom + ", Âge: " + age;
     }
+    public void afficherAmis() {
+        System.out.println(nom + " a " + amis.size() + " ami(s).");
+        for (Humain ami : amis) {
+            System.out.println(ami.nom + " " + ami.prenom);
+        }
+    }
+    public void ami(Humain humain) {
+        if (humain != null && !this.amis.contains(humain)) {
+            this.amis.add(humain);
+            humain.amis.add(this); // Ajoute une amitié mutuelle
+        }
+    }
+    
 }
